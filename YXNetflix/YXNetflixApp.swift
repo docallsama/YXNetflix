ed0627c353db376cd8f0d3c917b5d11ebf89e545
yixinxie <yixin_xie@singaporeair.com.sg>
2022-11-21 12:10:11 +0800

    initial commit

---
+++ YXNetflix/YXNetflixApp.swift
@@ -0,0 +1,17 @@
+//
+//  YXNetflixApp.swift
+//  YXNetflix
+//
+//  Created by BJSadmin on 2022/11/21.
+//
+
+import SwiftUI
+
+@main
+struct YXNetflixApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
